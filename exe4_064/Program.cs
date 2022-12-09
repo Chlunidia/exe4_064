0e8262e231bba03e8f0ba84c600897dc592ac101
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-12-09 15:37:23 +0700

    Create variable ele, top, max

--- exe4_064/Program.cs
+++ exe4_064/Program.cs
@@ -5,6 +5,9 @@ namespace StackUsingArray
     // Create  class Stacks
     class Stacks
     {
-
+        // Create variable ele, top, max
+        private int[] ele;
+        private int top;
+        private int max;
     }
 }
